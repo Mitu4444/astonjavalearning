7256feb4f2d306e5250ba76d72b142fc6e696c89
NafDevSora <mitu5005@gmail.com>
2026-08-24 18:04:50 +0300

    add streamAPI pipeline modifications data

--- src/module/two/Main.java
+++ src/module/two/Main.java
@@ -1,4 +1,36 @@
 package module.two;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args){
+
+        String path = System.getProperty("user.dir");
+        System.out.println(path);
+
+
+        List<Student> students = new Parser("src/module/two/studentandbooks.txt").parsing();
+
+        students.stream()
+                .peek(System.out::println)
+                .flatMap(s -> s.getBooks().stream().toList().stream())
+                .peek(System.out::println)
+                .sorted(Comparator.comparingInt(Book::getCountPage))
+                .peek(System.out::println)
+                .distinct()
+                .filter(b -> b.getYearRelease() > 2000)
+                .limit(3)
+                .map(Book::getYearRelease)
+                .findFirst()
+                .ifPresentOrElse(
+                        y -> System.out.println(y),
+                        () -> System.out.println("Такого значения нет")
+                        );
+        ;
+    }
 }
--- src/module/two/Parser.java
+++ src/module/two/Parser.java
@@ -1,4 +1,59 @@
 package module.two;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+
 public class Parser {
+
+    private final String filePath;
+
+    public Parser(final String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Student> parsing() {
+
+        String line;
+        HashSet<Student> students = new HashSet<Student>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            Student current = null;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if (data.length == 3) {
+                    long studentId = Long.parseLong(data[0]);
+                    String firstName = data[1];
+                    String lastName = data[2];
+
+                    current = new Student(studentId, firstName, lastName, new ArrayList<Book>());
+                    students.add(current);
+
+                } else if (data.length == 4) {
+
+                    String name = data[0];
+                    String author = data[1];
+                    int yearRelease = Integer.parseInt(data[2]);
+                    int countPage = Integer.parseInt(data[3]);
+
+                    if (current == null) continue;
+
+                    current.getBooks().add(new Book(name, author, yearRelease, countPage));
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return students.stream().toList();
+    }
 }
